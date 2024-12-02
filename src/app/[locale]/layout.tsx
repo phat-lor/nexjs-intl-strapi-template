708d7b1f3289db000cd71082d352dbc563401c0c
Pat <153535555+phat-lor@users.noreply.github.com>
2024-12-02 22:18:29 +0700

    fixes linting problem

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,16 +12,15 @@ export default async function LocaleLayout({
 	params,
 }: {
 	children: React.ReactNode;
-	params: { locale: string };
+	params: Promise<{ locale: string }>;
 }) {
 	const { locale } = await params;
-	// Ensure that the incoming `locale` is valid
+
 	// @ts-expect-error locale is a string
 	if (!routing.locales.includes(locale)) {
 		notFound();
 	}
-	// Providing all messages to the client
-	// side is the easiest way to get started
+
 	const messages = await getMessages();
 
 	return (
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@nextui-org/theme";
 import { ReactNode } from "react";
 
 const MaxWidthWrapper = ({
